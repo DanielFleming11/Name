3c0d2a59a9a9c7811e900da3504907f86cfe275a
DanielFleming11 <72047073+DanielFleming11@users.noreply.github.com>
2020-10-26 17:25:09 -0400

    ForLoop
    while loop in comments// for loop for the code.

---
+++ ForLoop.py
@@ -0,0 +1,30 @@
+# Less than 10 lines of code needed #
+'''
+x = 1
+total = 0
+a = 0
+
+while (a == 0) :
+    if x == 10:
+        print (total)
+        break 
+    else:
+        total = total + x
+        x = x + 1
+
+
+'''
+
+x = 1
+total = 0
+
+
+for x in range(0,11):
+    
+    if x == 10:
+        print(total)
+        break
+    else:
+        total = total + x
+        x = x + 1
+        
